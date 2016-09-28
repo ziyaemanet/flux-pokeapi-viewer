6ba253012c6b68ab3f864f2f8d021f6eff425546
Ziya Emanet <ziyaemanet@Ziyas-MacBook-Pro.local>
2016-09-28 12:05:10 -0700

    fix https

--- src/API.js
+++ src/API.js
@@ -3,14 +3,14 @@ import ServerActions from './actions/ServerActions';
 
 const API = {
   fetchPokemon(pokemon){
-    $.get(`http://pokeapi.co/api/v2/pokemon/${pokemon}`,(data) => {
+    $.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`,(data) => {
       //debugger;
       ServerActions.recievePokemon(data);
     });
   },
 
   fetchPokedex(){
-    $.get('http://pokeapi.co/api/v2/pokedex/1',(data) => {
+    $.get('https://pokeapi.co/api/v2/pokedex/1',(data) => {
       //debugger;
       ServerActions.recievePokedex(data);
     });
